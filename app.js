f64a81f76d604d5eb5fa29dde7c6c2282765e959
hisamc91237 <hisamc2000@gmail.com>
2021-09-02 20:07:04 +0530

    Data types

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log('Hey javascript')
-console.warn('Warning')
-console.error('error')
+// Data Types
+let age = 20
+let Name = 'Hisam'
+console.log(age)
+console.log(Name)
+console.log(typeof age)
+
+let brand 
+console.log(brand)
+
+let arr = [1 ,2 ,3 ,4]
+arr.push("value")
+arr.push(false)
+console.log(arr)
+console.log(typeof arr)
