419b192de26d756dacb457e95f2a091a3ba19550
hisamc91237 <hisamc2000@gmail.com>
2026-03-24 13:34:55 +0530

    addElement

--- Practice Folder/index.js
+++ Practice Folder/index.js
@@ -1,9 +1,23 @@
-let box = document.querySelector("div");
 let btn = document.querySelector("button");
+let main = document.querySelector("main");
 
 btn.addEventListener("click", () => {
-  let red = Math.floor(Math.random() * 255);
-  let green = Math.floor(Math.random() * 255);
-  let blue = Math.floor(Math.random() * 255);
-  box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+
+  let a = Math.floor(Math.random() * 100);
+  let b = Math.floor(Math.random() * 100);
+  let c = Math.floor(Math.random() * 360);
+
+  let div = document.createElement("div");
+  main.appendChild(div);
+  div.style.height = "100px";
+  div.style.width = " 100px";
+  div.style.backgroundColor = `rgb(${x},${y},${z})`;
+  div.style.position = "absolute";
+  div.style.top = a + "%";
+  div.style.left = b + "%";
+  div.style.rotate = c + "deg";
+  div.style.overflow = "none";
 });
